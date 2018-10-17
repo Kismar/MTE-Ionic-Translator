b0e3d92b18f9600043a6655746ba82ffd2bedfd5
Student <student@iMac-B202-02.local>
2018-10-17 16:24:31 +0200

    show translation result in UI

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TranslationProvider } from '../../providers/translation/translation';
 
 @Component({
   selector: 'page-home',
@@ -9,11 +10,22 @@ export class HomePage {
 
 
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    private translationService: TranslationProvider) {
+    
   }
 
+  public translated = '';
+  public uinput = '';
   public btnTranslate(input: string): void {
     console.log(input);
+    this.translationService.getTranslation(input).subscribe(
+      (response) => {
+        console.log(response.responseData.translatedText);
+        this.uinput = input;
+        this.translated = response.responseData.translatedText;
+      }
+    )
   }
 }
